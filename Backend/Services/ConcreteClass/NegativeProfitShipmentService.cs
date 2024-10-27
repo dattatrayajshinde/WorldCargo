44dee4fb11d5f88052eacdf9d27c9795bcb9162d
dattatrayajshinde <55657095+dattatrayajshinde@users.noreply.github.com>
2024-10-27 08:53:53 -0400

    Add files via upload

---
+++ Backend/Services/ConcreteClass/NegativeProfitShipmentService.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldCargoRepository.Models;
+using WorldCargoRepository.Wrapper;
+using Microsoft.EntityFrameworkCore;
+using Services.DTO.Request;
+using Services.DTO.Response;
+using Services.Interface;
+using Services.Wrapper;
+namespace Services.ConcreteClass
+{
+    public class NegativeProfitShipmentService: BaseService,INegativeProfitShipmentService
+    {
+        public NegativeProfitShipmentService()
+        {
+        }
+
+        public Task<bool> DeleteNegativeProfitShipments(int id)
+        {
+            var result = _repo.ProfitShipment.DeleteShipmentData(id);
+            return result;
+        }
+
+        public List<NegativeProfitShipmentDTO> GetNegativeProfitShipments()
+        {
+            List<NegativeProfitShipmentDTO> dtos = new List<NegativeProfitShipmentDTO>();
+            var shipments = _repo.ProfitShipment.GetAllNegativeShipmentData().ToList();
+            shipments.ForEach((x) =>
+            {
+                dtos.Add(new NegativeProfitShipmentDTO {
+                 //  EDT=x.EDT,
+                   CreatedBy=x.CreatedBy,
+                   CreatedDate=x.CreatedDate,
+                 //  DepartmentName=x.DepartmentName,
+                //   ETA=x.ETA,
+                   FileNo=x.FileNo,
+                   Id= x.Id,
+                   MawbNo=x.MawbNo,
+                //   ModifiedBy=x.ModifiedBy,
+                //   ModifiedDate=x.ModifiedDate,
+                   Profit= x.Profit
+                });
+            });
+            return dtos.ToList();
+        }
+
+        public List<NegativeProfitShipmentDTO> AddNegativeProfitShipments(NegativeProfitShipmentRequestModel model)
+        {
+            List<NegativeProfitShipmentDTO> dtos = new List<NegativeProfitShipmentDTO>();
+
+            NegativeProfitShipment dataRequest = new NegativeProfitShipment()
+            {
+                //  EDT = model.EDT,
+                CreatedBy ="admin",
+                CreatedDate =Convert.ToDateTime(DateTime.Now.ToString("HH:mm:ss")),
+                //  DepartmentName = model.DepartmentName,
+                //  ETA = model.ETA,
+                FileNo = model.FileNo,
+                //Id = model.Id,
+                MawbNo = model.MawbNo,
+                //  ModifiedBy = model.ModifiedBy,
+                //  ModifiedDate = model.ModifiedDate,
+                Profit = model.Profit
+            };
+
+            var shipments = _repo.ProfitShipment.AddNegativeShipmentData(dataRequest).ToList();
+            shipments.ForEach((x) =>
+            {
+                dtos.Add(new NegativeProfitShipmentDTO
+                {
+                    //  EDT=x.EDT,
+                    CreatedBy = x.CreatedBy,
+                    CreatedDate = x.CreatedDate,
+                    //  DepartmentName=x.DepartmentName,
+                    //   ETA=x.ETA,
+                    FileNo = x.FileNo,
+                    Id = x.Id,
+                    MawbNo = x.MawbNo,
+                    //   ModifiedBy=x.ModifiedBy,
+                    //   ModifiedDate=x.ModifiedDate,
+                    Profit = x.Profit
+                });
+            });
+            return dtos.ToList();
+        }
+
+        public List<NegativeProfitShipmentDTO> UpdateNegatieShipment(NegativeProfitShipmentRequestModel model)
+        {
+
+            List<NegativeProfitShipmentDTO> dtos = new List<NegativeProfitShipmentDTO>();
+            NegativeProfitShipment dataRequest = new NegativeProfitShipment()
+            {
+              //  EDT = model.EDT,
+                CreatedBy = model.CreatedBy,
+                CreatedDate = model.CreatedDate,
+              //  DepartmentName = model.DepartmentName,
+              //  ETA = model.ETA,
+                FileNo = model.FileNo,
+                Id = model.Id,
+                MawbNo = model.MawbNo,
+              //  ModifiedBy = model.ModifiedBy,
+              //  ModifiedDate = model.ModifiedDate,
+                Profit = model.Profit
+            };
+            var shipments = _repo.ProfitShipment.UpdateNegativeShipmentData(dataRequest).ToList();
+
+            shipments.ForEach((x) =>
+            {
+                dtos.Add(new NegativeProfitShipmentDTO
+                {
+                   // EDT = x.EDT,
+                    CreatedBy = x.CreatedBy,
+                    CreatedDate = x.CreatedDate,
+                  //  DepartmentName = x.DepartmentName,
+                 //   ETA = x.ETA,
+                    FileNo = x.FileNo,
+                    Id = x.Id,
+                    MawbNo = x.MawbNo,
+                  //  ModifiedBy = x.ModifiedBy,
+                  //  ModifiedDate = x.ModifiedDate,
+                    Profit = x.Profit
+                });
+            });
+            return dtos.ToList();
+        }
+
+    }
+}
---
+++ Backend/Services/Interface/INegativeProfitShipmentService.cs
@@ -0,0 +1,22 @@
+﻿using WorldCargoRepository.Models;
+using Services.DTO.Request;
+using Services.DTO.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interface
+{
+    public interface INegativeProfitShipmentService
+    {
+        List<NegativeProfitShipmentDTO> AddNegativeProfitShipments(NegativeProfitShipmentRequestModel model);
+        List<NegativeProfitShipmentDTO> GetNegativeProfitShipments();
+
+        Task<bool> DeleteNegativeProfitShipments(int id);
+
+        List<NegativeProfitShipmentDTO> UpdateNegatieShipment(NegativeProfitShipmentRequestModel model);
+
+    }
+}
